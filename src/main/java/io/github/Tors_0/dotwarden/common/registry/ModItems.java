bbb11d7a5f4d1821b9de0feb2d1c26299c9dc00d
rae <raej0000@gmail.com>
2023-10-22 00:11:34 -0700

    yay recipes and models

--- src/main/java/io/github/Tors_0/dotwarden/common/registry/ModItems.java
+++ src/main/java/io/github/Tors_0/dotwarden/common/registry/ModItems.java
@@ -18,29 +18,35 @@ public interface ModItems {
 
 	// Item NAME = createItem("name", new ModItem(new QuiltItemSettings()));
     // Basic Utility
-	Item POWER_OF_THE_DISCIPLE = createItem("power_of_the_disciple", new PowerItem(
-		new QuiltItemSettings().maxCount(1).group(ItemGroup.TOOLS).rarity(Rarity.EPIC)));
-    Item SCULKED_KNIFE = createItem("sculked_knife", new SculkedKnifeItem(new QuiltItemSettings().maxCount(1)
-            .group(ItemGroup.COMBAT).rarity(Rarity.RARE).maxDamage(64)));
+	Item POWER_OF_THE_DISCIPLE      = createItem("power_of_the_disciple",       new PowerItem(
+            new QuiltItemSettings().maxCount(1).group(ItemGroup.TOOLS).rarity(Rarity.EPIC)));
+    Item SCULKED_KNIFE              = createItem("sculked_knife",               new SculkedKnifeItem(
+            new QuiltItemSettings().maxCount(1).group(ItemGroup.COMBAT).rarity(Rarity.RARE).maxDamage(64)));
     // Crafting Materials
-    Item CORRUPTED_HEART = createItem("corrupted_heart", new CorruptedHeartItem(new QuiltItemSettings()
-            .rarity(Rarity.RARE).maxCount(1).fireproof().group(ItemGroup.MISC)));
-    Item CAPTURED_SOUL = createItem("captured_soul", new SoulItem(new QuiltItemSettings().group(ItemGroup.MISC).maxCount(4).rarity(Rarity.RARE)));
-    Item SCULK_CORE = createItem("sculk_core", new Item(new QuiltItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
-    Item ECHO_CHAMBER = createItem("echo_chamber", new EchoChamberItem(new QuiltItemSettings().group(ItemGroup.TOOLS).maxCount(1).rarity(Rarity.RARE)));
-    // Custom Items
-    Item HARMONIC_STAFF = createItem("harmonic_staff", new HarmonicStaffItem(new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1).rarity(Rarity.RARE)));
-    Item SEISMIC_HORN = createItem("seismic_horn", new SeismicHornItem(new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1).rarity(Rarity.RARE)));
-//  Item SCULK_BELL = createItem("sculk_bell", new Item(new QuiltItemSettings().group(ItemGroup.COMBAT)));
+    Item CORRUPTED_HEART            = createItem("corrupted_heart",             new CorruptedHeartItem(
+            new QuiltItemSettings().rarity(Rarity.RARE).maxCount(1).fireproof().group(ItemGroup.MISC)));
+    Item CAPTURED_SOUL              = createItem("captured_soul",               new SoulItem(
+            new QuiltItemSettings().group(ItemGroup.MISC).maxCount(4).rarity(Rarity.RARE)));
+    Item SCULK_CORE                 = createItem("sculk_core",                  new Item(
+            new QuiltItemSettings().group(ItemGroup.MISC).maxCount(1).rarity(Rarity.RARE)));
+    Item ECHO_CHAMBER               = createItem("echo_chamber",                new EchoChamberItem(
+            new QuiltItemSettings().group(ItemGroup.TOOLS).maxCount(1).rarity(Rarity.RARE)));
+    // Custom Items/Weapons
+    Item HARMONIC_STAFF             = createItem("harmonic_staff",              new HarmonicStaffItem(
+            new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1).rarity(Rarity.RARE)));
+    Item SEISMIC_HORN               = createItem("seismic_horn",                new SeismicHornItem(
+            new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1).rarity(Rarity.RARE)));
+/*    Item SCULK_BELL                 = createItem("sculk_bell",                  new Item(
+            new QuiltItemSettings().group(ItemGroup.COMBAT)));*/
     // Armor Items
-    Item DISCIPLE_HOOD = createItem("disciple_helmet", new DiscipleArmorItem(EquipmentSlot.HEAD,
-        new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1)));
-    Item DISCIPLE_CLOAK = createItem("disciple_chestplate", new DiscipleArmorItem(EquipmentSlot.CHEST,
-        new QuiltItemSettings().group(ItemGroup.COMBAT)));
-    Item DISCIPLE_GREAVES = createItem("disciple_leggings", new DiscipleArmorItem(EquipmentSlot.LEGS,
-        new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1)));
-    Item DISCIPLE_BOOTS = createItem("disciple_boots", new DiscipleArmorItem(EquipmentSlot.FEET,
-        new QuiltItemSettings().group(ItemGroup.COMBAT)));
+    Item DISCIPLE_HOOD              = createItem("disciple_helmet",             new DiscipleArmorItem(EquipmentSlot.HEAD,
+            new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1)));
+    Item DISCIPLE_CLOAK             = createItem("disciple_chestplate",         new DiscipleArmorItem(EquipmentSlot.CHEST,
+            new QuiltItemSettings().group(ItemGroup.COMBAT)));
+    Item DISCIPLE_GREAVES           = createItem("disciple_leggings",           new DiscipleArmorItem(EquipmentSlot.LEGS,
+            new QuiltItemSettings().group(ItemGroup.COMBAT).maxCount(1)));
+    Item DISCIPLE_BOOTS             = createItem("disciple_boots",              new DiscipleArmorItem(EquipmentSlot.FEET,
+            new QuiltItemSettings().group(ItemGroup.COMBAT)));
 
     /**
      * Creates a new item of type T using the constructor from T
